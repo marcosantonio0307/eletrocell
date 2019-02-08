a44a6c1ec2d6723d41238fc44d435292b452000f
Marcos Antonio <marcosantoniotaina@gmail.com>
2019-02-08 14:11:52 -0300

    Concluido secao de estoque principal

--- app/controllers/produtos_controller.rb
+++ app/controllers/produtos_controller.rb
@@ -8,16 +8,48 @@ class ProdutosController < ApplicationController
 		@produto = Produto.new
 	end
 
+	def busca
+		@nome = params[:nome]
+		@todos = Produto.where "nome like ?", "%#{@nome}%"
+	end
+
 	def create
 		valores = params.require(:produto).permit!
-		produto = Produto.create valores
-		redirect_to produtos_estoque_url
+		@produto = Produto.create valores
+		if @produto.save
+			redirect_to produtos_estoque_path, notice: "Produto Criado Com Sucesso!"
+		else
+			render :new
+		end
+	end
+
+	def edit
+		@produto = Produto.find(params[:id])
+		render :edit
+	end
+
+	def update
+		@produto = Produto.find(params[:id])
+		valores = params.require(:produto).permit!
+		@produto.update valores
+		redirect_to produtos_estoque_path
+	end
+
+	def entrada
+		@produto = Produto.find(params[:id])
+	end
+
+	def somaquantidade
+		entrada = params[:entrada].to_i
+		@produto = Produto.find(params[:id])
+		@produto.update(quantidade: @produto.quantidade + entrada)
+		redirect_to produtos_estoque_path
 	end
 
 	def destroy
 		id = params[:id]
 		Produto.destroy id
-		redirect_to produtos_estoque_url
+		redirect_to produtos_estoque_path, notice: "Produto Excluído!"
 	end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,10 @@ Rails.application.routes.draw do
   get "produtos/estoque" => "produtos#estoque"
   get "produtos/new" => "produtos#new"
   delete "produtos/:id" => "produtos#destroy", as: :produto
+  get "produtos/busca" => "produtos#busca", as: :busca_produto
+  get "produtos/:id/edit" => "produtos#edit", as: :edit_produto
+  get "produtos/:id/entrada" => "produtos#entrada", as: :entrada_produto
+  post "produtos/:id/entrada" => "produtos#somaquantidade"
+  patch "produtos/:id" => "produtos#update"
   root "produtos#index"
 end
